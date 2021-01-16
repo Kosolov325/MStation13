815de6baadc0c8e2f41b5ea9a41af727b235db75
Kosolov325 <xxlucas325xx@gmail.com>
2021-01-16 20:24:03 +0000

    html fix
    aa

--- config/motd/motd.js
+++
@@ -1,8 +0,0 @@
-function updatemenu() {
-  if (document.getElementById('responsive-menu').checked == true) {
-    document.getElementById('menu').style.borderBottomRightRadius = '0';
-    document.getElementById('menu').style.borderBottomLeftRadius = '0';
-  }else{
-    document.getElementById('menu').style.borderRadius = '2px';
-  }
-}
